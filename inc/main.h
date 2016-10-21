3a797b7c0a084af93d27f6d7d9f9d87e3539be21
snoopy <snoopy831002@gmail.com>
2016-10-21 10:45:24 +0800

    add include file

---
+++ inc/main.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+//include stdint.h for unit32_t
+#include <limits.h>
+//include limits.f for UNIT_MAX
+int clz(uint32_t x);
+//uint32_t count = 16;
+//uint32_t mask  = 0xFFFFFFFF;
+//int result=0;
